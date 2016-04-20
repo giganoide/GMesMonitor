4992a09f21cb591dcb1d50af3cf2028e3d7bc778
giganoide <giganoide@hotmail.com>
2016-04-20 17:31:24 +0200

    Add MainWindowTile (DX)

--- GMesMonitor/GMesMonitor/MainWindow.xaml.cs
+++ GMesMonitor/GMesMonitor/MainWindow.xaml.cs
@@ -1,12 +1,13 @@
 ﻿using System.Windows;
 using GMesMonitor.ViewModel;
+using DevExpress.Xpf.Core;
 
 namespace GMesMonitor
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : DXWindow
     {
         /// <summary>
         /// Initializes a new instance of the MainWindow class.
